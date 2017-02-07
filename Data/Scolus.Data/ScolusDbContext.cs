f0fe80f3bbffae66e2d93719b59df6e05c51385b
franciscebu <francis.cebu@basecamptech.ph>
2017-02-07 21:12:04 +0800

    - initialize solution
    - created data project
    - added db context

---
+++ Data/Scolus.Data/ScolusDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scolus.Data
+{
+    public class ScolusDbContext : DbContext
+    {
+        //TODO : Connection string
+        public ScolusDbContext() : base("")
+        {
+
+        }
+    }
+}
